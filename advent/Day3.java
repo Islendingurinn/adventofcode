1d7e2b4e5d8c0e24085c5a5f7d951af9b51567f9
Islendingurinn <37486706+Islendingurinn@users.noreply.github.com>
2018-12-06 00:41:54 +0100

    Update Day3.java

--- advent/Day3.java
+++ advent/Day3.java
@@ -2,13 +2,16 @@ package me.islend.advent;
 
 import java.awt.Point;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day3 extends Advent{
 
+	private Set<Fabric> fabriclist;
+	private int[][] size;
+	
 	public static void main(String[] args) throws IOException {
 		new Day3();
 	}
@@ -20,87 +23,70 @@ public class Day3 extends Advent{
 	@Override
 	protected void setup() {
 		
+		fabriclist = new HashSet<>();
+		for(String str : getInput()) {
+			int id = Integer.parseInt(str.split(" ")[0].replace("#", ""));
+			
+			int xpoint = Integer.parseInt(str.split(" ")[2].split(",")[0]);
+			int ypoint = (Integer.parseInt(str.split(" ")[2].split(",")[1].replace(":", "")));
+			
+			int width = Integer.parseInt(str.split(" ")[3].split("x")[0]);
+			int height = Integer.parseInt(str.split(" ")[3].split("x")[1]);
+			
+			fabriclist.add(new Fabric(id, xpoint, ypoint, width, height));
+		}
+		
+		size = new int[1000][1000];
 	}
 
 	@Override
 	protected Object solveFirst() {
-		List<Point> takenpoints = new ArrayList<Point>();
-		List<Point> duplicates = new ArrayList<Point>();
-		int fabric = 0;
+		int fabriccount = 0;
 		
-		for(String str : getInput()) {
-			int xpoint = Integer.parseInt(str.split(" ")[2].split(",")[0]) + 1;
-			int ypoint = -(Integer.parseInt(str.split(" ")[2].split(",")[1].replace(":", "")) + 1);
-			
-			int width = Integer.parseInt(str.split(" ")[3].split("x")[0]);
-			int height = Integer.parseInt(str.split(" ")[3].split("x")[1]);
-			
-			//System.out.println("xpoint: " + xpoint + ", ypoint: " + ypoint + ", width: " + width + ", height: " + height);
-			
-			for(int y = 0; y < height; y++) {
-				
-				for(int x = 0; x < width; x++) {
+		for(Fabric fabric : fabriclist) {
+			for(int y = fabric.ypoint; y < (fabric.ypoint + fabric.ydim); y++) {
+				for(int x = fabric.xpoint; x < (fabric.xpoint + fabric.xdim); x++) {
 					
-					Point point = new Point(xpoint + x, ypoint - y);
-					if(takenpoints.contains(point) && !duplicates.contains(point)) {
-						fabric++;
-						duplicates.add(point);
-					}else {
-						takenpoints.add(point);
+					if(size[x][y]++ == 1) {
+						fabriccount++;
 					}
-					
 				}
 			}
 		}
-		
-		return fabric;
+	
+		return fabriccount;
 	}
 
 	@Override
 	protected Object solveSecond() {
-		HashMap<String, List<Point>> candidate = new HashMap<String, List<Point>>();
-		List<Point> allpoints = new ArrayList<Point>();
+		HashMap<Integer, List<Point>> candidate = new HashMap<>();
 		
-		for(String str : getInput()) {
-			String id = str.split(" ")[0];
-			
-			int xpoint = Integer.parseInt(str.split(" ")[2].split(",")[0]) + 1;
-			int ypoint = -(Integer.parseInt(str.split(" ")[2].split(",")[1].replace(":", "")) + 1);
-			
-			int width = Integer.parseInt(str.split(" ")[3].split("x")[0]);
-			int height = Integer.parseInt(str.split(" ")[3].split("x")[1]);
-			
-			List<Point> candidatepoints = new ArrayList<Point>();
-			for(int y = 0; y < height; y++) {
-				
-				for(int x = 0; x < width; x++) {
-
-					Point point = new Point(xpoint + x, ypoint - y);
-					allpoints.add(point);
-					candidatepoints.add(point);
-					
-				}
-			}
-			
-			candidate.put(id, candidatepoints);
-		}
-		
-		List<String> idstoremove = new ArrayList<String>();
-		for(String id : candidate.keySet()) {
-			for(Point p : candidate.get(id)) {
-				System.out.println(Collections.frequency(allpoints, p));
-					if(!(Collections.frequency(allpoints, p) == 1)) {
-						idstoremove.add(id);
-						break;
+		loop: for(Fabric fabric : fabriclist) {
+			for(int y = fabric.ypoint; y < (fabric.ypoint + fabric.ydim); y++) {
+				for(int x = fabric.xpoint; x < (fabric.xpoint + fabric.xdim); x++) {
+					if(size[x][y] == 1) {
+						continue loop;
 					}
 				}
 			}
-		
-		for(String removeid : idstoremove) {	
-			candidate.remove(removeid);
+			
+			return fabric.id;
 		}
 		
-		return candidate.toString();
+		return candidate.get(0);
 	}
-
+	
+	private class Fabric {
+		int id, xpoint, ypoint, xdim, ydim;
+		
+		Fabric(int id, int xpoint, int ypoint, int xdim, int ydim){
+			this.id = id;
+			this.xpoint = xpoint;
+			this.ypoint = ypoint;
+			this.xdim = xdim;
+			this.ydim = ydim;
+		}
+	}
 }
+
+	
